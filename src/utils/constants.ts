4fef9d3e3e287fe53f5bb69bb6b656193a01a9e1
Shocam7 <87086200+Shocam7@users.noreply.github.com>
2025-08-10 20:17:13 +0530

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const OPENROUTER_CHAT_ENDPOINT = 'https://api.openrouter.ai/v1/chat/completions';
 export const OPENROUTER_COMPLETIONS_ENDPOINT = 'https://api.openrouter.ai/v1/completions';
-export const MISTRAL_MODEL = 'mistral-small-3.1-24b-instruct:free';
-export const DEEPSEEK_MODEL = 'deepseek-chat-v3-0324:free';
+export const MISTRAL_MODEL = 'mistralai/mistral-small-3.1-24b-instruct:free';
+export const DEEPSEEK_MODEL = 'deepseek/deepseek-chat-v3-0324:free';
 export const LLAMA_MAVERICK_MODEL = 'meta-llama/llama-4-maverick:free';
